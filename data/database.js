ab40b2afa09ddd77b14cc18c257450774ff2700c
Vahid <vahidhusyn@gmail.com>
2023-08-23 10:52:43 +0530

    updated database host

--- data/database.js
+++ data/database.js
@@ -7,6 +7,6 @@ mongoose
     .connect(process.env.MONGO_URI,{
         dbName: "backendapi", 
     })
-    .then(()=> console.log("Database connected"))
+    .then((c)=> console.log(`Database connected with ${c.connection.host}`))
     .catch((e)=> console.log(e));
 };
