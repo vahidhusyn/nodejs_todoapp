b61880d8e54a97e62f84456fd9ce6c690d589f1e
Vahid <vahidhusyn@gmail.com>
2023-08-24 16:01:24 +0530

    changed user.js for CORS error

--- controllers/user.js
+++ controllers/user.js
@@ -58,8 +58,8 @@ export const logout = (req,res)=>{
     res
     .status(200)
     .cookie("token", "", {expires: new Date(Date.now()),
-        // sameSite: process.env.NODE_ENV=== "Development" ? "lax":"none",
-        // secure:process.env.NODE_ENV==="Development" ? false : true,  
+        sameSite: process.env.NODE_ENV=== "Development" ? "lax":"none",
+        secure:process.env.NODE_ENV==="Development" ? false : true,  
     })
     .json({
         success: true,
